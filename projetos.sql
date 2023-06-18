736e5f2ab44602a6571767d2f7bec45739c298cf
deborasous <deborapatriciadp27@gmail.com>
2023-06-18 15:09:23 -0300

    [M2S06] Ex 6 - Modelando uma nova classe: Categoria

--- projetos.sql
+++ projetos.sql
@@ -4,14 +4,21 @@ CREATE TABLE Projetos (
   nome VARCHAR(100),
   descricao VARCHAR(100),
   data_inicio DATE,
-  data_prazo DATE
+  data_prazo DATE,
+  categoria_id INTEGER,
+  Foreign Key (categoria_id) REFERENCES Categorias(id)
 );
 CREATE TABLE Tarefas(
   id INTEGER PRIMARY KEY,
   nome VARCHAR(100),
   data_inicio DATE,
   data_prazo DATE,
-  concluida VARCHAR
+  concluida VARCHAR(100),
   projeto_id INTEGER,
   Foreign Key (projeto_id) REFERENCES Projetos(id)
+);
+CREATE TABLE Categorias(
+  id INTEGER PRIMARY KEY,
+  nome VARCHAR(100),
+  descricao VARCHAR(100)
 );
